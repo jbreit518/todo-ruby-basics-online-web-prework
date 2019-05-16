26bb0c126a3f0dc3397698bb67c9336bb68bc9e3
Justine Berretta <justberretta@gmail.com>
2019-05-16 15:45:15 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,23 @@
 def division(num1, num2)
- 
+  returns (#{num1} / #{num2}) 
 end
+
 
 def assign_variable(value)
-  
+  puts "#{value}"
 end
 
-def argue
-  
+def argue(phrase)
+  returns "#{phrase}"
 end
 
-def greeting
+def greeting(greeting, name)
+  puts "#{greeting}, #{name}"
 
 end
 
 def return_a_value
-
+  returns "Nice"
 end
 
 def last_evaluated_value
